5bcca269bcb38f33cbc39cec742c67eda5a54548
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-09 17:14:57 +0100

    chiedo all'utente un numero e stabilisco quando perde

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ Se il numero è presente nella lista dei numeri generati, la partita termina, al
 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 */
+
 //Funzione per generare numero random
 
 function randomNumber( min, max) {
  return parseInt(Math.random() * (max - min) + min + 1) ;
 }
-
 //Funzione per cercare in un array
 function isInArray(array, item) {
   var i = 0;
@@ -41,3 +41,22 @@ while (numbersRandom.length < 16) {
   }
 }
 console.log( numbersRandom );
+//Inizio ad interagire con l'utente
+//Creo delle variabili
+var numeroUtente;
+var tentativi = 5;
+var found = false;
+var score = 0;
+while ( numeriUtente.length < tentativi && found == false ) {
+//Chiedo all'utente un numero da 1 a 100
+var numeroUtente = parseInt(prompt('Inserisci un numero tra 0 e 100'));
+//Stabilisco le condizioni
+  //Se l'utente digita un numero che si trova nell'array allora l'utente ha perso
+  if (isInArray(numbersRandom, numeroUtente) == true ) {
+    alert('hai preso la mina! hai perso.');
+    found == true;
+  
+  } else {
+
+  }
+ }
